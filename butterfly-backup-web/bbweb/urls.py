b6e557e85a5a6aeed1165b144101623511aa6110
Matteo Guadrini <matteo.guadrini@hotmail.it>
2025-06-18 08:54:15 +0200

    chore: add home function for homepage

--- butterfly-backup-web/bbweb/urls.py
+++ butterfly-backup-web/bbweb/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from bbweb import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('', views.home, name='home'),
 ]
---
+++ butterfly-backup-web/bbweb/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.template import loader
+from bbweb.settings import CATALOG_PATH
+from bb import read_catalog
+import os
+
+
+# region exceptions
+class CatalogError(Exception): ...
+
+
+# endregion
+
+
+def home(request):
+    catalog_file = os.path.join(CATALOG_PATH, ".catalog.cfg")
+    if not os.path.exists(catalog_file):
+        raise CatalogError(f"catalog doesn't exists: {catalog_file}")
+    backups = dict()
+    config = read_catalog(catalog_file)
+    for section in config.sections():
+        backups[section] = {
+            "name": config.get(section, "name", fallback=None),
+            "type": config.get(section, "type", fallback=None),
+            "os": config.get(section, "os", fallback=None),
+            "timestamp": config.get(section, "timestamp", fallback=None),
+            "start": config.get(section, "start", fallback=None),
+            "end": config.get(section, "end", fallback=None),
+            "status": config.get(section, "status", fallback="0"),
+        }
+    template = loader.get_template("home.html")
+    context = {
+        "backups": backups,
+        "catalog": catalog_file,
+    }
+    return HttpResponse(template.render(context, request))
