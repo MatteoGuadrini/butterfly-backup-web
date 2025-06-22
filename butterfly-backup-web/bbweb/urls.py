8707d610eee4d743609bbef578fbe61831ba8cdb
Matteo Guadrini <matteo.guadrini@hotmail.it>
2025-06-22 10:20:07 +0200

    chore: add details path

--- butterfly-backup-web/bbweb/urls.py
+++ butterfly-backup-web/bbweb/urls.py
@@ -22,5 +22,6 @@ from bbweb import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("details/<str:section>", views.details, name="details"),
 ]
