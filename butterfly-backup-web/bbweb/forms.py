3ea29040fe5af6c144fc96898c8036ed66ca3df5
Matteo Guadrini <matteo.guadrini@hotmail.it>
2025-07-03 10:09:04 +0200

    chore: add initial value to backup form

--- butterfly-backup-web/bbweb/forms.py
+++ butterfly-backup-web/bbweb/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class BackupForm(forms.Form):
     computer = forms.CharField(label="Computer name or ip", max_length=100)
-    user = forms.CharField(label="Username", max_length=100)
-    port = forms.IntegerField(label="SSH port number")
+    user = forms.CharField(label="Username", max_length=100, initial="root")
+    port = forms.IntegerField(label="SSH port number", initial=22)
     mode = forms.ChoiceField(
         choices=(
             ("1", "full"),
@@ -14,6 +14,7 @@ class BackupForm(forms.Form):
         ),
         label="Mode",
         required=True,
+        initial="2",
     )
     data = forms.ChoiceField(
         choices=(
@@ -21,7 +22,7 @@ class BackupForm(forms.Form):
             ("2", "config"),
             ("3", "application"),
             ("4", "system"),
-            ("5", "log")
+            ("5", "log"),
         ),
         label="Data",
         required=True,
@@ -31,15 +32,17 @@ class BackupForm(forms.Form):
             ("1", "unix"),
             ("2", "macos"),
             ("3", "windows"),
-        ),      
+        ),
         label="OS type",
         required=True,
     )
-    retention_days = forms.IntegerField(label="Retention days")
-    retention_number = forms.IntegerField(label="Retention minimum number")
-    compress = forms.BooleanField(label="Compress")
-    skip_error = forms.BooleanField(label="Skip error")
-    checksum = forms.BooleanField(label="Checksum")
-    acl = forms.BooleanField(label="Preserve ACL")
-    retry = forms.IntegerField(label="Retry number")
-    wait = forms.IntegerField(label="Seconds of retry wait")
+    retention_days = forms.IntegerField(label="Retention days", required=False)
+    retention_number = forms.IntegerField(
+        label="Retention minimum number", required=False
+    )
+    compress = forms.BooleanField(label="Compress", required=False)
+    skip_error = forms.BooleanField(label="Skip error", required=False)
+    checksum = forms.BooleanField(label="Checksum", required=False)
+    acl = forms.BooleanField(label="Preserve ACL", required=False)
+    retry = forms.IntegerField(label="Retry number", required=False)
+    wait = forms.IntegerField(label="Seconds of retry wait", required=False)
